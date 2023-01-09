6511dc8e9135db14d48e28c50d09e4a536ede9e8
Pavel ZHIRONKIN <latahbah@e1r7p16.clusters.42paris.fr>
2023-01-09 13:19:45 +0100

    Normed builtin.c, builtin2.c and environ.c
    Normed all builtins. For it split on two files.
    Also normed environs. For it put set_env in set_env.c file, splited on few funcs
    and edited 'tmp' var

---
+++ src/builtin2.c
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin2.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: latahbah <latahbah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/09 12:07:37 by latahbah          #+#    #+#             */
+/*   Updated: 2023/01/09 12:35:15 by latahbah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "exec.h"
+
+/******************************************************************************
+ * 
+ * Need to test ft_export(), ft_env(), ft_unset()
+ * 
+ *****************************************************************************/
+
+int	ft_env(char **args, char ***env)
+{
+	int	i;
+
+	(void) args;
+	i = 0;
+	while ((*env)[i])
+	{
+		printf("%s\n", (*env)[i]);
+		i++;
+	}
+	printf("Env count = %d\n", i);
+	return (0);
+}
+
+/******************************************************************************
+ * find index of '=' separator to split on key-value
+ * @param str a string where to find '='
+ * @return	j (index of '=') if success, 
+ * 			-1 if there isn't '=' in the string
+ * 			-2 if there is a var name error
+ *****************************************************************************/
+
+static int	get_stop(const char *str)
+{
+	int	j;
+	int	stop;
+
+	stop = -1;
+	j = 0;
+	while (str[j])
+	{
+		if (str[0] == '?' && str[1] == '=')
+			return (1);
+		else if (str[j] == '=' && j != 0)
+			return (j);
+		else if ((str[j] < 65 || (str[j] > 90
+					&& str[j] < 97) || str[j] > 122) && stop == -1)
+		{
+			printf("export error: invalid symbols\n");
+			return (-2);
+		}
+		j++;
+	}
+	return (-1);
+}
+
+int	ft_export(char **args, char ***env)
+{
+	char	*key;
+	char	*value;
+	int		i;
+	int		stop;
+
+	i = 1;
+	while (args[i])
+	{
+		stop = get_stop(args[i]);
+		if (stop == -2)
+			return (-100);
+		else if (stop > -1)
+		{
+			key = ft_substr(args[i], 0, stop);
+			value = ft_substr(args[i], stop + 1, ft_strlen(args[i]) - stop);
+		}
+		else
+		{
+			key = args[i];
+			value = "";
+		}
+		set_env(env, key, value);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_unset(char **args, char ***env)
+{
+	int		i;
+	int		j;
+	size_t	index;
+
+	(void) args;
+	(void) env;
+	i = 1;
+	while (args[i])
+	{
+		j = 0;
+		while ((*env)[j])
+		{
+			if (!ft_strncmp(args[i], (const char *)(*env)[j],
+				ft_strlen(args[i])))
+			{
+				index = (size_t) j;
+				realloc_environ(env, count_env(env) - 1, index);
+				break ;
+			}
+			j++;
+		}
+		i++;
+	}	
+	return (0);
+}
--- src/environ.c
+++ src/environ.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   environ.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgarcia <jgarcia@student.42.fr>            +#+  +:+       +#+        */
+/*   By: latahbah <latahbah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 01:11:08 by jgarcia           #+#    #+#             */
-/*   Updated: 2023/01/06 01:11:49 by jgarcia          ###   ########.fr       */
+/*   Updated: 2023/01/09 12:50:34 by latahbah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ size_t	count_env(char ***env)
 {
 	size_t	i;
 
-	if(env == NULL || *env == NULL)
+	if (env == NULL || *env == NULL)
 		return (0);
 	i = 0;
 	while ((*env)[i])
@@ -40,7 +40,7 @@ void	del_environ(char ***env)
 {
 	size_t	i;
 
-	if(*env == NULL)
+	if (*env == NULL)
 		return ;
 	i = 0;
 	while ((*env)[i])
@@ -55,120 +55,42 @@ void	del_environ(char ***env)
 /******************************************************************************
  * Reallocate the environment array to size size
  * [UPDATE]:Recode realloc func to decrease size of env. Index is the env[index]
- * 			element to unset. If we add element, index is -1.
+ * 			element to unset. If we add element, index is -1. (also i deleted
+ * 			size <= count_env(dev) condition in the first if statement) 
  * @param env Pointer to Environment
  * @param size New size of the environment array
  * @param index Index of element needs to unset
  * @return 0 if success, -1 if error
  *****************************************************************************/
 
-int	realloc_environ(char ***env,size_t size, size_t index)
+int	realloc_environ(char ***env, size_t size, size_t index)
 {
 	char	**new_env;
 	size_t	i;
 	size_t	j;
 
-	if (env == NULL || size == 0) // deleted "size <= count_env(env) condition"
+	if (env == NULL || size == 0)
 		return (-1);
 	new_env = malloc(sizeof(char *) * (size + 1));
 	if (new_env == NULL)
 		return (-1);
 	i = 0;
 	j = 0;
-	// printf("\nIn realloc\n");
-	// printf("\tNew env size = %ld\n", size);
-	// printf("\tIndex = %ld\n", index);
 	while (j < size && (*env)[i])
 	{
-		// printf("\ti = %ld\n", i);
 		if (index == i)
-		{
-			// printf("\n\nHERE\n\n");
 			i++;
-		}
 		new_env[j] = ft_strdup((*env)[i]);
 		i++;
 		j++;
 	}
 	new_env[i] = NULL;
-	// printf("\n\n\tnew_env[1] = [%p]\n\tenv[1] = [%p]\n\n", new_env[1], env[1]);
 	del_environ(env);
 	*env = new_env;
-	// printf("Mistake out the realloc\n");
-	return (0);
-}
-/******************************************************************************
- * Add a variable to the environment
- * @param env Pointer to Environment
- * @param name Name of the variable
- * @param value Value of the variable
- * @return 0 if success, -1 if error
- *****************************************************************************/
-
-int	set_env(char ***env,char *key,char *value)
-{
-	size_t	i;
-	size_t	size;
-	char	*tmp;
-
-	i=0;
-	//FOR TESTING
-	// printf("\tin set_env()\n");
-	// printf("\tkey = %s\n\tvalue = %s\n", key, value);
-	// int j = 0;
-	// while (*env[j])
-	// {
-	// 	printf("%s\n", *env[j]);	
-	// 	j++;
-	// }
-	// printf("\tbefore set_env() count = %d\n", j);
-	//
-	if ((*env) == NULL)
-	{
-		(*env) = malloc(sizeof(char *) * 2);
-		if ((*env) == NULL)
-			return (-1);
-		tmp = ft_strjoin(key, "=");
-		(*env)[0] = ft_strjoin(tmp, value);
-		(*env)[1] = NULL;
-		return (0);
-	}
-	while ((*env)[i] != NULL)
-	{
-		tmp = ft_strjoin(key,"=");
-		if (tmp == NULL)
-			return (-1);
-		if (ft_strncmp(tmp,(*env)[i],ft_strlen(tmp)) == 0)
-			break;
-		i ++;
-	}
-	size = count_env(env);
-	if (i > size -1)
-	{
-		realloc_environ(env, size + 1, -1);
-		if (!(*env))
-			return (-1);
-		(*env)[size] = ft_strjoin(tmp,value);
-	}
-	else
-	{
-		if ((*env)[i])
-			free((*env)[i]);
-		(*env)[i] = ft_strjoin(tmp,value);
-	}
-	//FOR TESTING
-	// j = 0;
-	// while (env[j])
-	// {
-	// 	j++;	
-	// }
-	// printf("\tafter set_env() count = %d\n", j);
-	//
-	free(tmp);
 	return (0);
 }
 
-char	*get_env(char *key,char ***env)
+char	*get_env(char *key, char ***env)
 {
 	size_t	i;
 	char	*tmp;
@@ -179,10 +101,10 @@ char	*get_env(char *key,char ***env)
 	i = 0;
 	while ((*env)[i])
 	{
-		tmp = ft_strjoin(key,"=");
-		if (ft_strncmp(tmp,(*env)[i],ft_strlen(tmp)) == 0)
+		tmp = ft_strjoin(key, "=");
+		if (ft_strncmp(tmp, (*env)[i], ft_strlen(tmp)) == 0)
 		{
-			ret = ft_substr((*env)[i],ft_strlen(tmp),ft_strlen((*env)[i]));
+			ret = ft_substr((*env)[i], ft_strlen(tmp), ft_strlen((*env)[i]));
 			free(tmp);
 			return (ret);
 		}
