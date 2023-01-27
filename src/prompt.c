e55f674ef820173523992a5807d25052d37d49da
corecaps <corecaps@gmail.com>
2023-01-27 17:12:17 +0100

    norme

--- src/prompt.c
+++ src/prompt.c
@@ -6,11 +6,27 @@
 /*   By: jgarcia <jgarcia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 12:54:16 by jgarcia           #+#    #+#             */
-/*   Updated: 2023/01/21 02:33:11 by jgarcia          ###   ########.fr       */
+/*   Updated: 2023/01/27 17:12:02 by jgarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	check_return_code(char **return_code, char **open, char **close)
+{
+	if (ft_atoi((*return_code)) == 0)
+	{
+		(*open) = ft_strdup("[\033[32m");
+		free((*return_code));
+		(*return_code) = ft_strdup("✓");
+		(*close) = ft_strdup("\033[0m]");
+	}
+	else
+	{
+		(*open) = ft_strdup("[\033[31m");
+		(*close) = ft_strdup("\033[0m]");
+	}
+}
 
 char	*get_return_code(char ***env)
 {
@@ -22,18 +38,7 @@ char	*get_return_code(char ***env)
 	return_code = get_env("?", env);
 	if (!return_code)
 		return (NULL);
-	if (ft_atoi(return_code) == 0)
-	{
-		open = ft_strdup("[\033[32m");
-		free(return_code);
-		return_code = ft_strdup("✓");
-		close = ft_strdup("\033[0m]");
-	}
-	else
-	{
-		open = ft_strdup("[\033[31m");
-		close = ft_strdup("\033[0m]");
-	}
+	check_return_code(&return_code, &open, &close);
 	tmp = ft_strjoin(open, return_code);
 	free(open);
 	open = ft_strjoin(tmp, close);
@@ -56,41 +61,36 @@ char	*get_user(char ***env)
 	return (tmp);
 }
 
+void	prompt_join(char **prompt, char *tmp, int dir)
+{
+	char	*tmp2;
+
+	if (dir == 1)
+		tmp2 = ft_strjoin(tmp, *prompt);
+	else
+		tmp2 = ft_strjoin(*prompt, tmp);
+	free(*prompt);
+	free(tmp);
+	*prompt = ft_strdup(tmp2);
+	free(tmp2);
+}
+
 char	*get_prompt(char ***env)
 {
 	char	*prompt;
 	char	*tmp;
-	char	*tmp2;
 
 	prompt = ft_strdup("\033[0;32m[minishell]");
 	tmp = get_return_code(env);
 	if (tmp)
-	{
-		tmp2 = ft_strjoin(tmp, prompt);
-		free(prompt);
-		prompt = ft_strdup(tmp2);
-		free(tmp);
-		free(tmp2);
-	}
+		prompt_join(&prompt, tmp, 1);
 	tmp = get_user(env);
 	if (tmp)
-	{
-		tmp2 = ft_strjoin(prompt, tmp);
-		free(prompt);
-		free(tmp);
-		prompt = ft_strdup(tmp2);
-		free(tmp2);
-	}
-	tmp2 = getcwd(NULL, 0);
-	if (tmp2)
-	{
-		tmp = ft_strjoin(prompt, tmp2);
-		free(tmp2);
-	}
-	else
-		tmp = ft_strdup(prompt);
-	tmp2 = ft_strjoin(tmp, "$\033[0m ");
-	free(tmp);
-	free(prompt);
-	return (tmp2);
+		prompt_join(&prompt, tmp, 2);
+	tmp = getcwd(NULL, 0);
+	if (tmp)
+		prompt_join(&prompt, tmp, 2);
+	tmp = ft_strdup("$\033[0m ");
+	prompt_join(&prompt, tmp, 2);
+	return (prompt);
 }
