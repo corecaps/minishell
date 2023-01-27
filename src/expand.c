6aa2a02d8aa4072417d61ec0ddbe093ffc0a9a18
Pavel ZHIRONKIN <latahbah@e1r2p21.clusters.42paris.fr>
2023-01-27 14:21:03 +0100

    expander.c norme done

--- src/expand.c
+++ src/expand.c
@@ -6,7 +6,7 @@
 /*   By: latahbah <latahbah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 16:35:29 by latahbah          #+#    #+#             */
-/*   Updated: 2023/01/27 12:49:18 by latahbah         ###   ########.fr       */
+/*   Updated: 2023/01/27 14:19:58 by latahbah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,14 +58,30 @@ static char	*retrieve_str(char *tmp, char ***env)
  * Initialisation of indexes to norme
  ****************************************************************************/
 
-t_exp	exp_init(int i)
+t_exp	*exp_init(int num)
 {
-	t_exp	exp;
+	t_exp	*exp;
 
-	exp.i = i;
-	exp.start = i;
-	exp.flag = 0;
+	exp = malloc(sizeof(t_exp));
+	exp->i = num;
+	exp->start = num;
+	exp->flag = 0;
+	exp->tmp = NULL;
+	exp->result = NULL;
 	return (exp);
+}
+
+void	update_exp_res(t_exp *exp, char *str, char ***env)
+{
+	exp->tmp = ft_substr(str, exp->start, (size_t)(exp->i - exp->start));
+	if (exp->flag == 1)
+		exp->tmp = retrieve_str(exp->tmp, env);
+	exp->result = add_to_res(exp->result, exp->tmp);
+	exp->start = exp->i;
+	if (str[exp->i] == '$')
+		exp->flag = 1;
+	else
+		exp->flag = 0;
 }
 
 /*****************************************************************************
@@ -74,37 +90,24 @@ t_exp	exp_init(int i)
 
 char	*expand(char *str, char ***env)
 {
-	t_exp	exp;
-	char	*result;
-	char	*tmp;
+	t_exp	*exp;
 	char	c;
+	char	*result;
 
 	exp = exp_init(0);
-	result = ft_strjoin("", "");
-	while (str[exp.i])
+	exp->result = ft_strjoin("", "");
+	while (str[exp->i])
 	{
-		c = str[exp.i];
+		c = str[exp->i];
 		if (c == '$' || c == ' ' || c == '\t' || c == '='
 			|| c == '\n' || c == '\v' || c == '\f' || c == '\r')
-		{
-			tmp = ft_substr(str, exp.start, (size_t)(exp.i - exp.start));
-			if (exp.flag == 1)
-				tmp = retrieve_str(tmp, env);
-			result = add_to_res(result, tmp);
-			exp.start = exp.i;
-			if (c == '$')
-				exp.flag = 1;
-			else
-				exp.flag = 0;
-		}
-		++exp.i;
+			update_exp_res(exp, str, env);
+		++exp->i;
 	}
-	tmp = ft_substr(str, exp.start, (size_t)(exp.i - exp.start));
-	if (exp.flag == 1)
-	{
-		tmp = retrieve_str(tmp, env);
-	}
-	result = add_to_res(result, tmp);
+	update_exp_res(exp, str, env);
+	result = ft_strdup(exp->result);
+	free(exp->result);
+	free(exp);
 	free(str);
 	return (result);
 }
